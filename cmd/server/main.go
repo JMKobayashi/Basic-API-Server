b1b04f5727c531f6762f075493c20d5150b5af6b
JMKobayashi <james.kobayashi@alumni.usp.br>
2025-03-05 11:04:35 -0300

    Finalize product handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/JMKobayashi/Basic-API-Server/configs"
-	"github.com/JMKobayashi/Basic-API-Server/internal/dto"
 	"github.com/JMKobayashi/Basic-API-Server/internal/entity"
 	"github.com/JMKobayashi/Basic-API-Server/internal/infra/database"
+	"github.com/JMKobayashi/Basic-API-Server/internal/infra/webserver/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -24,39 +25,19 @@ func main() {
 
 	db.AutoMigrate(&entity.Product{}, &entity.User{})
 	productDB := database.NewProduct(db)
-	productHandler := NewProductHandler(productDB)
+	productHandler := handlers.NewProductHandler(productDB)
 
-	http.HandleFunc("/product", productHandler.CreateProduct)
-	http.ListenAndServe(":8000", nil)
-}
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB)
 
-type ProductHandler struct {
-	ProductDB database.ProductInterface
-}
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/product", productHandler.CreateProduct)
+	r.Get("/product/{id}", productHandler.GetProduct)
+	r.Put("/product/{id}", productHandler.UpdateProduct)
+	r.Delete("/product/{id}", productHandler.DeleteProduct)
+	r.Get("/products", productHandler.GetProducts)
 
-func NewProductHandler(db database.ProductInterface) *ProductHandler {
-	return &ProductHandler{
-		ProductDB: db,
-	}
-}
-
-func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
-	var product dto.CreateProductInput
-	err := json.NewDecoder(r.Body).Decode(&product)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	p, err := entity.NewProduct(product.Name, product.Price)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	err = h.ProductDB.Create(p)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusCreated)
+	r.Post("/user", userHandler.CreateUser)
+	http.ListenAndServe(":8000", r)
 }
