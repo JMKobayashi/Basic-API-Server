5fde997f76a7c51b64f11c2f97fad6bc351eff6b
JMKobayashi <james.kobayashi@alumni.usp.br>
2025-03-05 23:09:52 -0300

    Create a JWT Token

--- internal/infra/webserver/handlers/user_handlers.go
+++ internal/infra/webserver/handlers/user_handlers.go
@@ -3,18 +3,62 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/JMKobayashi/Basic-API-Server/internal/dto"
 	"github.com/JMKobayashi/Basic-API-Server/internal/entity"
 	"github.com/JMKobayashi/Basic-API-Server/internal/infra/database"
+	"github.com/go-chi/jwtauth"
 )
 
 type UserHandler struct {
-	UserDB database.UserInterface
+	UserDB       database.UserInterface
+	JWT          *jwtauth.JWTAuth
+	JWTExpiresIn int
 }
 
-func NewUserHandler(db database.UserInterface) *UserHandler {
-	return &UserHandler{UserDB: db}
+func NewUserHandler(db database.UserInterface, jwt *jwtauth.JWTAuth, JwtExpiresIn int) *UserHandler {
+	return &UserHandler{
+		UserDB:       db,
+		JWT:          jwt,
+		JWTExpiresIn: JwtExpiresIn}
+}
+
+func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
+	var user dto.GetJWTInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	u, err := h.UserDB.FindByEmail(user.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+
+	if !u.ValidatePassword(user.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	_, token, err := h.JWT.Encode(map[string]interface{}{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Second * time.Duration(h.JWTExpiresIn)).Unix(),
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	accessToken := struct {
+		AccessToken string `json:"access_token"`
+	}{
+		AccessToken: token,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(accessToken)
 }
 
 func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
