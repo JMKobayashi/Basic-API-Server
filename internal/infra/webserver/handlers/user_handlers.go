ef1c646bf7bb9205e721ea4511ba5fa6df2327c4
JMKobayashi <james.kobayashi@alumni.usp.br>
2025-03-08 10:48:40 -0300

    Add JWT generation and user/product API documentation

--- internal/infra/webserver/handlers/user_handlers.go
+++ internal/infra/webserver/handlers/user_handlers.go
@@ -11,6 +11,10 @@ import (
 	"github.com/go-chi/jwtauth"
 )
 
+type Error struct {
+	Message string `json:"message"`
+}
+
 type UserHandler struct {
 	UserDB database.UserInterface
 }
@@ -20,6 +24,17 @@ func NewUserHandler(db database.UserInterface) *UserHandler {
 		UserDB: db}
 }
 
+// Generate JWT godoc
+// @Summary Generate JWT Token
+// @Description Generate JWT Token
+// @Tags user
+// @Accept  json
+// @Produce  json
+// @Param request body dto.GetJWTInput true "user credentials"
+// @Success 200 {object} dto.GetJWTOutput
+// @Failure 404
+// @Failure 500 {object} Error
+// @Router /user/token [post]
 func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 	jwt := r.Context().Value("jwt").(*jwtauth.JWTAuth)
 	jwtExpiresIn := r.Context().Value("jwtExpiresIn").(int)
@@ -49,16 +64,22 @@ func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	accessToken := struct {
-		AccessToken string `json:"access_token"`
-	}{
-		AccessToken: token,
-	}
+	accessToken := dto.GetJWTOutput{AccessToken: token}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(accessToken)
 }
 
+// Create user godoc
+// @Summary Create a user
+// @Description Create a user
+// @Tags user
+// @Accept  json
+// @Produce  json
+// @Param request body dto.CreateUserInput true "user request"
+// @Success 201
+// @Failure 500 {object} Error
+// @Router /user [post]
 func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	var user dto.CreateUserInput
 	err := json.NewDecoder(r.Body).Decode(&user)
